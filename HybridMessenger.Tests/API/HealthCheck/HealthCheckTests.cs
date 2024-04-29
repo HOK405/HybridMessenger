8ec0680329fdcf9fdbbe75a25158a4b192094bcf
hok <boghdantereshenko@gmail.com>
2024-04-29 15:04:12 +0300

    Add health check integraion test

---
+++ HybridMessenger.Tests/API/HealthCheck/HealthCheckTests.cs
@@ -0,0 +1,26 @@
+﻿using HybridMessenger.API;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace HybridMessenger.Tests.API.HealthCheck
+{
+    public class HealthCheckTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public HealthCheckTests(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateDefaultClient();
+        }
+
+        [Fact]
+        public async Task HealthCheck_ReturnsOk()
+        {
+            // Arrange & Act
+            var response = await _httpClient.GetAsync("/health");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
