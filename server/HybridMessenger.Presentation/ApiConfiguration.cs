c6351f52561dbdf90d21e7571fc7038e0b65acef
hok <boghdantereshenko@gmail.com>
2024-06-05 17:34:50 +0300

    Separate client and back sides

---
+++ server/HybridMessenger.Presentation/ApiConfiguration.cs
@@ -0,0 +1,21 @@
+﻿namespace HybridMessenger.Presentation
+{
+    public static class ApiConfiguration
+    {
+        public static string ApiBaseAddress { get; set; } = "https://hybridmessengerapi20240524013739.azurewebsites.net/";
+        public static string HubEndpoint { get; set; } = "/chathub";
+        public static string FullHub => $"{ApiBaseAddress.TrimEnd('/')}/{HubEndpoint.TrimStart('/')}";
+    }
+
+    /*
+    In MauiProgram.cs:
+    string apiBaseAddress = builder.Configuration["ApiBaseAddress"];
+    */
+
+    /*
+    In ChatService:
+    string baseAddress = configuration.GetValue<string>("ApiBaseAddress");
+    string endpoint = configuration.GetValue<string>("HubEndpoint");
+    _url = $"{baseAddress.TrimEnd('/')}/{endpoint.TrimStart('/')}";
+    */
+}
